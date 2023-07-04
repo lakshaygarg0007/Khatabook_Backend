0a10459f7047f787e497475108d0011743d8e949
Evilgod12priyanshu <202151122@iiitvadodara.ac.in>
2023-07-04 21:23:02 +0530

    made a new api for total earning and expenses

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ const Expenses = require('./models/expenses');
 const PaymentMethods = require('./models/payment_methods');
 const parser = require('body-parser');
 var ObjectId = require('mongodb').ObjectId;
-
 app.use(cors());
 app.use(bodyParser.json());
 app.get('/', (req, res) => {
@@ -50,8 +49,8 @@ app.post('/addEarning', jsonParser, async function (req, res) {
     console.log(req.body);
     var earning = new Earnings(req.body);
     var id = new ObjectId(req.body.user_id)
-    const user = await User.find({ _id: id });
-    await User.updateOne({ _id: id }, { $set: { total_expense: parseFloat(user[0].total_expense) + req.body.amount } });
+    const user = await User.find({ _id: id }); 
+    await User.updateOne({ _id: id }, { $set: { total_earning: parseFloat(user[0].total_earning) + req.body.amount } });
     earning.save();
     res.send('Record Saved Successfully');
   } catch (error) {
@@ -191,41 +190,103 @@ app.post('/deleteExpense', jsonParser, async function (req, res) {
     req.send('Could Not Delete Earning');
   }
 });
-
-app.post('/getEarningAndExpense', jsonParser, async function(req, res) {
-  res.setHeader('Content-Type', 'application/json');
-  res.header("Access-Control-Allow-Origin", "*");
+app.get('/getEarningAndExpense', jsonParser, async function(req, res) {
+  // res.setHeader('Content-Type', 'application/json');
+  // res.setHeader("Access-Control-Allow-Origin", "*");
   try {
     const total_expense = await Expenses.aggregate([
       {$match: { user_id: req.body.user_id }},
       { $group: { _id: "$user_id", totalExpenses: { $sum: "$amount" } } }
-  ], function (err, result) {
-      if (err) {
-          console.log(err);
-          return;
-      }
-  });
+  ]
+  // , 
+  // function (err, result) {
+  //     if (err) {
+  //         console.log(err);
+  //         return;
+  //     }
+  // }
+  );
 
   const total_earning = await Earnings.aggregate([
       {$match: { user_id: req.body.user_id }},
       { $group: { _id: "$user_id", totalEarning: { $sum: "$amount" } } }
-  ], function (err, result) {
-      if (err) {
-          console.log(err);
-          return;
-      } 
-  });
+  ]
+  // , function (err, result) {
+  //     if (err) {
+  //         console.log(err);
+  //         return;
+  //     } 
+  // }
+  );
 
   const earning = total_earning[0].totalEarning
   const expense = total_expense[0].totalExpenses
+  console.log(earning)
+  console.log(expense)
+  console.log(total_expense)
+  console.log(total_earning)
 
   const response = {
-    earning, expense
+    total_expenses: total_expense.length > 0 ? total_expense[0].totalExpenses : 0,
+    total_earnings: total_earning.length > 0 ? total_earning[0].totalEarning : 0
   }
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.send(JSON.stringify(response));
 
   } catch (error) {
-    res.send('Could Not Get Earning And Expense');
-    throw error;
+    res.status(500).send('Could Not Get Earning And Expense');
+    // throw error;
+    console.log(error);
   }
 });
+// app.get('/getTotalEarning', async function (req, res) {
+//   const filter = { "_id": req.id };
+//   const all = await User.find(filter, { total_earning: 1, _id: 1 });
+//   res.setHeader('content-type', 'application/json');
+//   res.header("Access-Control-Allow-Origin", "*");;
+//   res.send(JSON.stringify(all));
+// });
+app.get('/getEarnAndExpens', jsonParser, async function(req, res){
+  res.setHeader('content-type', 'application/json');
+     res.header("Access-Control-Allow-Origin", "*");
+     
+    //  const filter = { "_id": "$user_id" };
+    //  const total_expense = await Expenses();
+    // const total_expense = 600;
+    //  const total_earning = await Earnings({ _id: "$user_id", totalEarning: { $sum: "$amount" } });
+    // const total_earning = 700
+    //  const earning = total_earning[0].totalEarning;
+    //  const expense = total_expense[0].totalExpenses;
+    try {
+      const total_expense = await Expenses.find().then(expenses => {
+        return expenses.reduce((total, expense) => total + expense.amount, 0);
+      });
+  
+      const total_earning = await Earnings.find().then(earnings => {
+        return earnings.reduce((total, earning) => total + earning.amount, 0);
+      });
+
+    const response = {
+      total_expenses: {  
+        amount: total_expense
+      },
+      total_earnings: {  
+        amount: total_earning
+      }
+    };
+    
+
+    //  const response = {
+    //  total_expense,
+    //  total_earning
+    // }
+    console.log(JSON.stringify(response));
+    res.send(JSON.stringify(response));
+  }
+  catch (error) {
+    console.log(error);
+    res.status(500).send('Could Not Get Earnings And Expenses');
+  }
+}
+)
